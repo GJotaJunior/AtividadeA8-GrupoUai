4821f42184b9cc45a42a5a50ed3e4ec1ecb952f0
Pedro Paulo <pedropaulo.kuhn30@gmail.com>
2021-06-21 20:11:20 -0300

    Criado estrutura EmpregadoTerceirizado

---
+++ src/model/EmpregadoTerceirizado.java
@@ -0,0 +1,15 @@
+package model;
+
+public class EmpregadoTerceirizado extends Empregado {
+
+	private double adicional;
+
+	public double getAdicional() {
+		return adicional;
+	}
+
+	public void setAdicional(double adicional) {
+		this.adicional = adicional;
+	}
+
+}
