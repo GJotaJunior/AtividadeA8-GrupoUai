49b396a11540a4489647467c017a505a56d0fe38
Wanderson Gutemberg <wanderson.gutembergs@gmail.com>
2021-06-22 20:12:32 -0300

    Implementa empregado e empregadoTerceirizado

--- src/model/Empregado.java
+++ src/model/Empregado.java
@@ -2,36 +2,42 @@ package model;
 
 public class Empregado {
 
-	private String nome;
-	private int horas;
-	private double valorPorHora;
+    private String nome;
+    private int horas;
+    private double valorPorHora;
 
-	public String getNome() {
-		return nome;
-	}
+    public String getNome() {
+	return nome;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setNome(String nome) {
+	this.nome = nome;
+    }
 
-	public int getHoras() {
-		return horas;
-	}
+    public int getHoras() {
+	return horas;
+    }
 
-	public void setHoras(int horas) {
-		this.horas = horas;
-	}
+    public void setHoras(int horas) {
+	if (horas > 40)
+	    throw new IllegalArgumentException("Os funcionários podem trabalhar por no máximo 40 horas.");
 
-	public double getValorPorHora() {
-		return valorPorHora;
-	}
+	this.horas = horas;
+    }
 
-	public void setValorPorHora(double valorPorHora) {
-		this.valorPorHora = valorPorHora;
-	}
-	
-	public double realizarPagamento() {
-		return 0.0;
-	}
+    public double getValorPorHora() {
+	return valorPorHora;
+    }
+
+    public void setValorPorHora(double valorPorHora) {
+	if (valorPorHora < 30.0 || valorPorHora > 200.0)
+	    throw new IllegalArgumentException("O valor por hora precisa ser um valor entre 30,00 e 200,00.");
+
+	this.valorPorHora = valorPorHora;
+    }
+
+    public double realizarPagamento() {
+	return (horas * valorPorHora) > 1100.0 ? horas * valorPorHora : 1100.0;
+    }
 
 }
--- src/model/EmpregadoTerceirizado.java
+++ src/model/EmpregadoTerceirizado.java
@@ -2,18 +2,22 @@ package model;
 
 public class EmpregadoTerceirizado extends Empregado {
 
-	private double adicional;
+    private double adicional;
 
-	public double getAdicional() {
-		return adicional;
-	}
+    public double getAdicional() {
+	return adicional;
+    }
 
-	public void setAdicional(double adicional) {
-		this.adicional = adicional;
-	}
+    public void setAdicional(double adicional) {
+	if (adicional < 100.0 || adicional > 1100.0)
+	    throw new IllegalArgumentException("O adicional precisa ser um valor entre 100,00 e 1.100,00.");
 
-	@Override
-	public double realizarPagamento() {
-		return super.realizarPagamento();
-	}
+	this.adicional = adicional;
+    }
+
+    @Override
+    public double realizarPagamento() {
+	return (getHoras() * getValorPorHora() + adicional) > 1100.0 ? getHoras() * getValorPorHora() + adicional
+		: 1100.0;
+    }
 }
